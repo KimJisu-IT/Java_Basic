095465154180e4838a32487dd03b348b852d6d60
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-09-13 20:28:50 +0900

    [Add] 한줄 추가

--- src/main/java/javabasic/demo/FirstApplication.java
+++ src/main/java/javabasic/demo/FirstApplication.java
@@ -3,5 +3,6 @@ package javabasic.demo;
 public class FirstApplication {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello");
     }
 }
